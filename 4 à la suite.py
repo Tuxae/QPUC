dd42d8ae2cb3cb1090c2933c38d5feb0ab75c1b4
oreo <jacques.zhang@ensae.fr>
2023-12-04 15:11:48 +0100

    Update graphisme 4 a la suite

--- 4 à la suite.py
+++ 4 à la suite.py
@@ -20,6 +20,16 @@ def draw_rounded_rect(screen, rect, color, border_color, border_radius):
     pygame.draw.rect(screen, border_color, rect, border_radius=border_radius)
     pygame.draw.rect(screen, border_color, rect, width=2)  # Width of 2 for the border
 
+def draw_outlined_text(font, text, color, outline_color, pos):
+    text_surface = font.render(text, True, color)
+    outline_surface = font.render(text, True, outline_color)
+
+    text_rect = text_surface.get_rect(center=pos)
+    outline_rect = outline_surface.get_rect(center=pos)
+
+    screen.blit(outline_surface, outline_rect)
+    screen.blit(text_surface, text_rect)
+
 def draw_scores(screen, scores, selected_item, x=0, y=0):
     # Calculate the total height of the polygons
     total_height = len(scores) * 100
@@ -60,6 +70,11 @@ def draw_scores(screen, scores, selected_item, x=0, y=0):
     text_rect = text_surface.get_rect(center=(W // 2, H - 100))
     screen.blit(text_surface, text_rect)
 
+
+def adjust_brightness(color, factor):
+    # Adjust the brightness of a color
+    return tuple(int(min(max(0, c * factor), 255)) for c in color)
+
 # Set up the display
 W = 1920
 H = 1080
@@ -67,7 +82,7 @@ screen = pygame.display.set_mode((W, H), pygame.FULLSCREEN)
 pygame.display.set_caption('Menu and Polygons Screen')
 
 # Menu items
-menu_items = ["THEME 1", "THEME 2", "THEME 3", "THEME MYSTERE"]
+menu_items = ["Le chant dans le monde", "Sainteté et canonisation", "Traditions du Pays basque", "Thème mystère"]
 selected_items = [False, False, False, False]
 
 # Scores for each polygon
@@ -94,30 +109,27 @@ while running:
     screen.fill((255, 255, 255))
 
     if menu_screen:
-        # Draw rounded rectangles for menu items
-        max_text_width = max(GAME_FONT.size(item)[0] for item in menu_items) + 40
+        max_text_width = max(GAME_FONT.size(item)[0] for item in menu_items) + 500
         max_text_height = GAME_FONT.size(max(menu_items, key=len))[1] + 20
-        vertical_gap = 20  # Adjust this value to set the gap between rectangles
+        vertical_gap = 20
 
         for i, item in enumerate(menu_items):
             is_last_menu = i == len(menu_items) - 1
             rect_height = max_text_height
-            start_menu_y = (1080 - len(menu_items) * rect_height - (len(menu_items) - 1) * vertical_gap) // 2  # Calculate starting y-coordinate
+            start_menu_y = (1080 - len(menu_items) * rect_height - (len(menu_items) - 1) * vertical_gap) // 2
 
             rect_top = start_menu_y + i * (rect_height + vertical_gap)
-            rect_color = (0, 255, 0) if is_last_menu else (255, 255, 0)
-            rect_border_color = (0, 0, 255) if is_last_menu else (255, 215, 0)  # Blue border for the last menu
-            border_radius = 20
+            rect_color = adjust_brightness((50, 205, 50) if is_last_menu else (255, 228, 54), 0.8) if selected_items[i] else ((50, 205, 50) if is_last_menu else (255, 228, 54))  # Adjust brightness to 80%
+            rect_border_color = adjust_brightness((50, 205, 50) if is_last_menu else (255, 228, 54), 0.8) if selected_items[i] else ((50, 205, 50) if is_last_menu else (255, 228, 54))
 
             draw_rounded_rect(screen, pygame.Rect(1920 // 2 - max_text_width // 2, rect_top, max_text_width, rect_height),
-                              rect_color, rect_border_color, border_radius)
+                              rect_color, rect_border_color, 20)
 
             font = BOLD_FONT if selected_items[i] else GAME_FONT
-            text_surface = font.render(item, True, GRAY_RGB if selected_items[i] else (0, 0, 0))
-            text_rect = text_surface.get_rect(center=(1920 // 2, rect_top + rect_height // 2))
-            screen.blit(text_surface, text_rect)
+            text_color = (255, 255, 255)
+            outline_color = (0, 0, 0)
+            draw_outlined_text(font, item, text_color, outline_color, (W // 2, rect_top + rect_height // 2))
     else:
-        # Draw the polygons and the selected menu item
         draw_scores(screen, scores, menu_items[selected_items.index(True)])
 
     # Update the display
