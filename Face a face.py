ef42bc3b7f29e3eb01e5241344f20cbe2d4c665d
tttienthinh <tranthuongtienthinh@gmail.com>
2023-12-15 19:04:04 +0100

    draw_socre et playerzone face a face

--- Face a face.py
+++ Face a face.py
@@ -8,13 +8,24 @@ from pygame import mixer
 pygame.init()
 # super_arduino = SuperArduino("/dev/cu.usbmodem14112101")
 super_arduino = ShadowSuperArduino("/dev/cu.usbmodem14112101")
-                               
+    
+
+# Liste de joueurs à changer si besoin
+LISTE_JOUEURS = [
+    ("Camille", "images/Camille.jpg"),
+    ("Cyril", "images/profrouge.jpg"),
+    ("Thibault", "images/profrouge.jpg"),
+    ("Daniel", "images/Daniel.jpg")
+]
+
+# Taille d'écran                           
 W = 1920
 H = 1080
 
 TITLE_SIZE = H//12
 
 GAME_FONT = pygame.font.SysFont('Comic Sans MS', TITLE_SIZE)
+SCORE_FONT = pygame.font.SysFont('Comic Sans MS', int(TITLE_SIZE*0.5))
 TEXT_FONT = pygame.font.Font(None, 60)
 BOLD_FONT = pygame.font.Font(None, 36)
 BOLD_FONT.set_bold(True)  # Set the font to bold
@@ -30,11 +41,22 @@ def write_title(screen, title, y_pos):
     text_rect = text_qpuc.get_rect(center=(W/2, y_pos))
     screen.blit(text_qpuc, text_rect)
 
-def draw_hexagon(screen, x, y, width=0, height=0):
+def draw_hexagon(screen, x, y, color=GOLD_RGB, width=100):
+    # height = width / 2
+    width_scale = width/100
+    height_scale = width/100
     pygame.draw.polygon(
         screen, 
-        ORANGE_RGB, 
-        ((x+65, y+350), (x+115, y+350), (x+140, y+375), (x+115, y+400), (x+65, y+400), (x+40, y+375), (x+65, y+350))
+        color, 
+        (
+            (x-25*width_scale, y+0), 
+            (x+25*width_scale, y+0), 
+            (x+50*width_scale, y+25*height_scale), 
+            (x+25*width_scale, y+50*height_scale), 
+            (x-25*width_scale, y+50*height_scale), 
+            (x-50*width_scale, y+25*height_scale), 
+            (x-25*width_scale, y+0)
+        )
     )
 
 def draw_polygon(screen, x, y, polygon_size, polygon_colour):
@@ -119,6 +141,82 @@ def draw_player_zone(screen, score, i=0):
     text_rect = text_surface.get_rect(center=(PLAYER_BORDER+ i*(PLAYER_BORDER+PLAYER_LONG) + PLAYER_LONG//2, H - 120))
     screen.blit(text_surface, text_rect)
 
+def draw_score(screen, i, val=0):
+    hexagon_width = W/10
+    color = GOLD_RGB
+    draw_hexagon(
+        screen, 
+        PLAYER_BORDER+i*(PLAYER_BORDER+PLAYER_LONG)+PLAYER_LONG/2, 
+        H-2*PLAYER_BORDER-hexagon_width,
+        color,
+        width=hexagon_width
+    )
+    draw_hexagon(
+        screen, 
+        PLAYER_BORDER+i*(PLAYER_BORDER+PLAYER_LONG)+PLAYER_LONG/2, 
+        H-2*PLAYER_BORDER-hexagon_width*0.96,
+        SEASHELL_RGB,
+        width=hexagon_width*0.84
+    )
+    draw_hexagon(
+        screen, 
+        PLAYER_BORDER+i*(PLAYER_BORDER+PLAYER_LONG)+PLAYER_LONG/2, 
+        H-2*PLAYER_BORDER-hexagon_width*0.95,
+        BLUE_RGB,
+        width=hexagon_width*0.8
+    )
+    x = PLAYER_BORDER+i*(PLAYER_BORDER+PLAYER_LONG)+PLAYER_LONG/2
+    hexagon_width = W/10
+    y = H-2*PLAYER_BORDER-3*hexagon_width/4
+    text_qpuc = SCORE_FONT.render(str(val), False, SEASHELL_RGB)
+    text_rect = text_qpuc.get_rect(center=(x, y))
+    screen.blit(text_qpuc, text_rect)
+
+def draw_player_zone(screen, score, i=0):
+    rectangle_height = H-5*TITLE_SIZE/2-2*PLAYER_BORDER
+    pygame.draw.rect(
+        screen, 
+        GOLD_RGB, 
+        pygame.Rect(
+            PLAYER_BORDER+i*(PLAYER_BORDER+PLAYER_LONG), 
+            5*TITLE_SIZE/2, 
+            PLAYER_LONG, 
+            rectangle_height
+        )
+    )
+    pygame.draw.rect(
+        screen, 
+        POWDER, 
+        pygame.Rect(
+            PLAYER_BORDER+i*(PLAYER_BORDER+PLAYER_LONG), 
+            5*TITLE_SIZE/2+rectangle_height*0.01, 
+            PLAYER_LONG, 
+            rectangle_height*0.98
+        )
+    )
+    angle = 0
+    target_width = PLAYER_LONG-1*PLAYER_BORDER
+    name, img_src = LISTE_JOUEURS[i]
+    img = pygame.image.load(img_src).convert()
+    rect = img.get_rect()
+    scale = target_width / rect.width
+    img = pygame.transform.rotozoom(img, angle, scale)
+    screen.blit(
+        img, 
+        (
+            1.5*PLAYER_BORDER+i*(PLAYER_BORDER+PLAYER_LONG), 
+            5*TITLE_SIZE/2+0.5*PLAYER_BORDER
+        )
+    )
+    x = PLAYER_BORDER+i*(PLAYER_BORDER+PLAYER_LONG)+PLAYER_LONG/2
+    hexagon_width = W/10
+    y = H-2*PLAYER_BORDER-1*hexagon_width/4
+    text_qpuc = SCORE_FONT.render(name, False, BLUE_RGB)
+    text_rect = text_qpuc.get_rect(center=(x, y))
+    screen.blit(text_qpuc, text_rect)
+
+    draw_score(screen, i, score)
+
 def draw_rounded_rect(screen, rect, color, border_color, border_radius):
     pygame.gfxdraw.box(screen, rect, color)
     pygame.draw.rect(screen, border_color, rect, border_radius=border_radius)
@@ -134,43 +232,14 @@ def draw_outlined_text(font, text, color, outline_color, pos):
     screen.blit(outline_surface, outline_rect)
     screen.blit(text_surface, text_rect)
 
-def draw_scores(screen, scores, current_polygon_index, border_index, x=0, y=0):
-    polygon_size = 2
-    polygon_spacing = 60 * polygon_size
-
-    total_height = len(scores) * polygon_spacing
-    start_y = (H - total_height) // 2
-
-    for i, score in enumerate(scores):
-        y_offset = start_y + i * polygon_spacing
-
-        points = [(x * polygon_size, y * polygon_size + y_offset) for x, y in
-                  [(50, 0), (100, 0), (125, 25),
-                   (100, 50), (50, 50), (25, 25), (50, 0)]]
-
-        # Fill color is GREEN_RGB for the selected polygon and POWDER_RGB for others
-        fill_color = GREEN_RGB if i == current_polygon_index else BLUE_RGB
-
-        # Border color logic
-        if score > border_index:
-            border_color = POWDER_RGB 
-        else:
-            border_color = GOLD_RGB
-
-        pygame.draw.polygon(screen, fill_color, points)
-        pygame.draw.lines(screen, border_color, True, points, 10)  # 10 is the border thickness
-
-        text_surface = TEXT_FONT.render(str(score), False, SEASHELL_RGB)
-        text_rect = text_surface.get_rect(center=(75 * polygon_size, y_offset + 25 * polygon_size))
-        screen.blit(text_surface, text_rect)
-
 def in_interval(item,list):
      return list[0] < item <= list[1]
 
 
 screen.fill(BLUE_RGB)
-write_title(screen, "Questionsd pour un champion", TITLE_SIZE/2)
+write_title(screen, "Questions pour un champion", TITLE_SIZE/2)
 write_title(screen, "Face à face", 3*TITLE_SIZE/2)
+
 
 score = [0, 0]
 points = [4, 3, 2, 1]
@@ -224,7 +293,6 @@ while True:
                     tic_sound.stop()
                     
     for event in pygame.event.get():
-        print(timer_active)
         if event.type == pygame.QUIT:
             quit()
         elif event.type == pygame.KEYDOWN:
@@ -289,6 +357,16 @@ while True:
             screen.fill(BLUE_RGB)
             write_title(screen, "Questions pour un champion", TITLE_SIZE/2)
             write_title(screen, "Face à face", 3*TITLE_SIZE/2)
+            pygame.draw.rect(
+                screen, 
+                GOLD, 
+                pygame.Rect(
+                    W//2-PLAYER_LONG//8, 
+                    5*TITLE_SIZE/2, 
+                    PLAYER_LONG//4, 
+                    TITLE_SIZE//10
+                )
+            )
             draw_player_zone(screen, score[0], i=0)
             draw_player_zone(screen, score[1], i=3)
             for i, point in enumerate(points):
@@ -318,7 +396,16 @@ while True:
         write_title(screen, "Face à face", 3*TITLE_SIZE/2)
         draw_player_zone(screen, score[0], i=0)
         draw_player_zone(screen, score[1], i=3)
-        
+        pygame.draw.rect(
+            screen, 
+            GOLD, 
+            pygame.Rect(
+                W//2-PLAYER_LONG//8, 
+                5*TITLE_SIZE/2, 
+                PLAYER_LONG//4, 
+                TITLE_SIZE//10
+            )
+        )
         for i, point in enumerate(points):
             polygon_size = 100
             polygon_height = 2 * (polygon_size * math.sin(math.pi / 6))
